c36e59aef5397009cebaf20dd69c432792ffbd0f
Suhail Ansari <suhaila@apconic.com>
2018-01-11 14:50:33 +0530

    Added componentWillReceiveProps

--- src/tables/simple-virtualized-table.js
+++ src/tables/simple-virtualized-table.js
@@ -1,49 +1,61 @@
-import React, { Component } from 'react';
-import { Column, Table, AutoSizer, SortDirection } from 'react-virtualized';
-import { Panel } from 'aos-base-ui';
-import _ from 'lodash';
-import 'react-virtualized/styles.css';
-import './table.css';
+import React, { Component } from "react";
+import { Column, Table, AutoSizer, SortDirection } from "react-virtualized";
+import { Panel } from "aos-base-ui";
+import _ from "lodash";
+import "react-virtualized/styles.css";
+import "./table.css";
 
 class SimpleVirtualizedTable extends Component {
   constructor(props) {
     super(props);
     const sortBy = this.getSortBy();
     const sortDirection = SortDirection.ASC;
-    const sortedList = this.sortList({ sortBy, sortDirection: SortDirection.ASC });
+    const sortedList = this.sortList({
+      sortBy,
+      sortDirection: SortDirection.ASC
+    });
     this.state = {
       sortBy,
       sortDirection,
-      sortedList,
+      sortedList
     };
+  }
+
+  componentWillReceiveProps(newProps) {
+    const { sortBy, sortDirection } = this.state;
+    const sortedList = this.sortList({ sortBy, sortDirection }, newProps.list);
+    this.setState({ sortedList });
   }
 
   getSortBy = () => {
     return this.props.sortBy ? this.props.sortBy : this.props.columns[0].key;
-  }
-  
-  getData = (list, index) => list[index]
+  };
+
+  getData = (list, index) => list[index];
 
   sort = ({ sortBy, sortDirection }) => {
-    const sortedList = this.sortList({ sortBy, sortDirection });
-    this.setState({ sortedList, sortDirection, sortBy });
-  }
-
-  sortList = ({ sortBy, sortDirection }) => {
-    let sortedList = [];
     const { list } = this.props;
-    sortedList = sortDirection === SortDirection.ASC ? _.orderBy(list, [sortBy], ['asc']) : _.orderBy(list, [sortBy], ['desc']);
+    const sortedList = this.sortList({ sortBy, sortDirection }, list);
+    this.setState({ sortedList, sortDirection, sortBy });
+  };
+
+  sortList = ({ sortBy, sortDirection }, list) => {
+    let sortedList = [];
+    sortedList =
+      sortDirection === SortDirection.ASC
+        ? _.orderBy(list, [sortBy], ["asc"])
+        : _.orderBy(list, [sortBy], ["desc"]);
     return sortedList;
-  }
+  };
 
   rowClassName = ({ index }) => {
     if (index < 0) {
-      return 'headerRow';
+      return "headerRow";
     }
-    return index % 2 === 0 ? 'evenRow' : 'oddRow';
-  }
+    return index % 2 === 0 ? "evenRow" : "oddRow";
+  };
 
-  noRowsRenderer = () => <div className="noRows">No rows</div>
+  noRowsRenderer = () => <div className="noRows">No rows</div>;
 
   render() {
     const { sortBy, sortDirection, sortedList } = this.state;
@@ -53,7 +65,7 @@ class SimpleVirtualizedTable extends Component {
       <div>
         <div className="row">
           <div className="col-lg-12 col-md-12 col-sm-12">
-            <Panel style={{ marginTop: '16px', height: 400 }} >
+            <Panel style={{ marginTop: "16px", height: 400 }}>
               <AutoSizer>
                 {({ width }) => (
                   <Table
@@ -70,7 +82,15 @@ class SimpleVirtualizedTable extends Component {
                     headerClassName="headerColumn"
                     rowClassName={this.rowClassName}
                   >
-                    {columns.map(element => <Column label={element.label} dataKey={element.key} width={250} flexGrow={1} className="exampleColumn" />)}
+                    {columns.map(element => (
+                      <Column
+                        label={element.label}
+                        dataKey={element.key}
+                        width={250}
+                        flexGrow={1}
+                        className="exampleColumn"
+                      />
+                    ))}
                   </Table>
                 )}
               </AutoSizer>
