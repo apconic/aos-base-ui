4fd899fd23a7fd58d2a1f41278d990d491eed029
Hrushikesh Prakash Lokhande <hrushikeshl>
2018-03-14 17:44:17 +0530

    Improved card component

--- src/card.js
+++ src/card.js
@@ -1,55 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Divider from 'material-ui/Divider';
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import { grey300 } from 'material-ui/styles/colors';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
 
-const BaseCard = (props) => (
-  <Card {...props}>
+const ExpandableCard = (props) => (
+  <Card
+    initiallyExpanded={props.initiallyExpanded ? true : false}
+  >
+  {props.title ? (
+    <CardHeader
+    title={props.title}
+    showExpandableButton
+    titleStyle={{ fontSize: '16pt' }}
+    style={props.gray ? { background: grey300 } : null}
+  />
+  ) : (<div />)}
+  <Divider />
     {props.children}
+    <CardText>
+      {props.text}
+    </CardText>
+    <CardActions>
+      {props.actions}
+    </CardActions>
   </Card>
 );
 
-BaseCard.propTypes = {
+ExpandableCard.propTypes = {
   children: PropTypes.node,
+  title: PropTypes.string,
+  initiallyExpanded: PropTypes.bool,
+  text: PropTypes.node,
+  actions: PropTypes.node,
+  gray: PropTypes.bool,
+  showExpandableButton: PropTypes.bool,
 };
 
-const BaseCardActions = (props) => (
-  <CardActions {...props} style={{ paddingBottom: 16 }}>
-    {props.children}
-  </CardActions>
-);
-
-BaseCardActions.propTypes = {
-  children: PropTypes.node,
-};
-
-
-const BaseCardHeader = (props) => (
-  <div>
-    <CardHeader {...props}>
-      {props.children}
-    </CardHeader>
-    <Divider />
-  </div>
-);
-
-BaseCardHeader.propTypes = {
-  children: PropTypes.node,
-};
-
-const BaseCardText = (props) => (
-  <CardText {...props}>
-    {props.children}
-  </CardText>
-);
-
-BaseCardText.propTypes = {
-  children: PropTypes.node,
-};
-
-export {
-  BaseCard,
-  BaseCardActions,
-  BaseCardHeader,
-  BaseCardText
-};
+export default ExpandableCard;
